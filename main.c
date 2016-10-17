e2547d12edde5596ebae8355dfec802aaac3cf46
robin <beemo@openmailbox.org>
2016-10-17 15:33:46 +0200

    finished first draft of math function

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
-int math() {
+#define Plus 10
+#define Min 11
+#define Times 12
+#define Div 13
 
+//input is an array of length 7;
+//10:+ 11:- 12:* 13:/
+int math(int arr[]) {
+  int i,current=0;
+  float stack[4];
+  for (i=0; i<7; i++) {
+    if (arr[i] < 10) {
+      stack[i] = arr[i];
+      current++;
+    } else {
+      switch (arr[i]){
+        case Plus:
+          stack[current-2] = stack[current-2] + stack[current-1];
+        break;
+        case Min:
+          stack[current-2] = stack[current-2] - stack[current-1];
+        break;
+        case Times:
+          stack[current-2] = stack[current-2] * stack[current-1];
+        break;
+        case Div:
+          stack[current-2] = stack[current-2] / stack[current-1];
+      
+      }
+      current--;
+    }
+  }
+  printf("%f %d\n",stack[0], (int)stack[0]);
+  if (stack[0] - (int)stack[0] == 0) {
+    return stack[0];
+  } else {
+    return -1;
+  }
 }
 
 void printp() {
@@ -9,5 +45,6 @@ void printp() {
 }
 
 int main() {
-  
+  int arr[7] = {1,2,3,4,Div,Plus,Plus};
+  printf("%d\n", math(arr));
 }
