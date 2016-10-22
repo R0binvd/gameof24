69e37ed4f7655214b426b2114f40334788dd2308
Giacomo <gcasoni@hotmail.it>
2016-10-22 16:54:35 +0200

    Printing function completed, there could be a way to save ourselves a lot of if checks, but it would make the printing function uneccessarly complicated to code in my opinion. Check it out anyway and let me know

--- main.c
+++ main.c
@@ -5,22 +5,44 @@
 #define Times 12
 #define Div 13
 
-int printedString[20]  //I'll make this one global so I can check if the combination I'm printing is the one I JUST printed, since the two formulas overlap sometimes
+int printedString[20];  //I'll make this one global so I can check if the combination I'm printing is the one I JUST printed, since the two formulas overlap sometimes
 
-void printLayout1(int printable[]){    //I first place the parenthesis where needed, then fill the blanks with a for loop
+int printLayout1(int printable[]){    //I first place the parenthesis where needed, then fill the blanks with a for loop
   int i, j=0, upto=7;
   for(i=0; i<7; i++) scanf("%d", &printable[i]);
-  if(printable[2]<12 && printable[4]>11){printedString[0]=40; printedString[4]=41; upto=9;}
-  if(printable[2]<12 && printable[4]<12 && printable[6]>11){printedString[0]=40; printedString[6]=41; upto=9;}
-  if(printable[2]>11 && printable[4]<12 && printable[6]>11){printedString[0]=40; printedString[6]=41; upto=9;}
+  if(printable[2]<12 && printable[4]>11){printedString[0]=-8; printedString[4]=-7; upto=9;}
+  if(printable[2]<12 && printable[4]<12 && printable[6]>11){printedString[0]=-8; printedString[6]=-7; upto=9;}
+  if(printable[2]>11 && printable[4]<12 && printable[6]>11){printedString[0]=-8; printedString[6]=-7; upto=9;}
   for(i=0; i<9; i++){
-     if(printedString[i]!=40 && printedString[i]!=41){
+     if(printedString[i]!=-8 && printedString[i]!=-7){
           if(j==0) printedString[i]=printable[0];
           else if(j%2!=0) printedString[i]=printable[j+1];
           else if(j%2==0) printedString[i]=printable[j-1];;
           j++;
         }
   }
+  return upto;
+}
+
+int printLayout2(int printable[]){
+  int i, j=0, upto=7;
+  if(printable[2]<12 && printable[5]<12 && printable[6]<12) return 0;                                              //checking if... 
+  if(printable[2]<12 && printable[5]>11) return 0;                                                                 //...already printed
+  if(printable[2]<12 && printable[6]>11){printedString[0]=-8; printedString[4]=-7; upto+=2;}                       //here i plave the parenthesis in the right spot...
+  if(printable[5]<12 && printable[6]>11){                                                                          //
+       if(upto==9)   {printedString[6]=-8; printedString[10]=-7; upto+=2;}                                         //
+       else          {printedString[4]=-8; printedString[8]=-7; upto+=2;}                                          //...all the way down here
+  }
+  if(upto==9){printedString[3]=printable[6]; printedString[1]=printable[2]; printedString[6]=printable[5];}
+  if(upto==11){printedString[5]=printable[6]; printedString[2]=printable[2]; printedString[8]=printable[5];}
+  for(i=0; i<upto; i++){
+       if(printedString[i]<10 && printedString[i]>0){
+           if(printable[j]<10) printedString[i]=printable[j];
+           else printedString[i]=printable[j+1];
+           j++;
+       }
+  }
+  return upto;
 }
 
 //input is an array of length 7;
@@ -57,11 +79,27 @@ int math(int arr[]) {
   }
 }
 
-//Rearrange and print reverse polish notation in normal notation, input is a 7 elements array 
+//Rearrange and print reverse polish notation in normal notation, input is a 7 elements array, also checks if string has been already printed 
 void printp(int sequence[]) {
-     if(sequence[4]>=10)  printLayout1(sequence);     //print the ab?c?d? form
-     else printLayout2(sequence);                     //print the ab?cd?? form
-}
+     int i, upto;
+     if(sequence[4]>=10)  upto=printLayout1(sequence);     //print the ab?c?d? form
+     else upto=printLayout2(sequence);                     //print the ab?cd?? form
+     for(i=0; i<upto; i++){
+         if(printedString[i]<10)  printf("%c", printedString[i]+48);
+         else{
+          switch(printedString[i]){
+            case 10: printf("+");
+            break;
+            case 11: printf("-");
+            break;
+            case 12: printf("*");
+            break;
+            case 13: printf("/");
+            break;
+          }
+         }
+     }
+}          
 
 int main() {
   //testcase for the math function, should output -1;
