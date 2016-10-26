06152c62a56c64ac3f80e3214847b06a9e6cd52a
robin <beemo@openmailbox.org>
2016-10-26 12:32:30 +0200

    changed all 10..13 to Add..Div, made Add..Div go from 10..13 to 11..14 because otherwise the input could not contain 10.

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define Add 10
-#define Sub 11
-#define Mul 12
-#define Div 13
+#define Add 11
+#define Sub 12
+#define Mul 13
+#define Div 14
 
 void swap(int *a, int *b) {
   int swap;
@@ -18,9 +18,9 @@ int printedString[20];  //I'll make this one global so I can check if the combin
 int printLayout1(int printable[]){    //I first place the parenthesis where needed, then fill the blanks with a for loop
   int i, j=0, upto=7;
   /*for(i=0; i<7; i++) scanf("%d", &printable[i]);*/
-  if(printable[2]<12 && printable[4]>11){printedString[0]=-8; printedString[4]=-7; upto=9;}
-  if(printable[2]<12 && printable[4]<12 && printable[6]>11){printedString[0]=-8; printedString[6]=-7; upto=9;}
-  if(printable[2]>11 && printable[4]<12 && printable[6]>11){printedString[0]=-8; printedString[6]=-7; upto=9;}
+  if(printable[2]<Mul && printable[4]>Sub){printedString[0]=-8; printedString[4]=-7; upto=9;}
+  if(printable[2]<Mul && printable[4]<Mul && printable[6]>Sub){printedString[0]=-8; printedString[6]=-7; upto=9;}
+  if(printable[2]>Sub && printable[4]<Mul && printable[6]>Sub){printedString[0]=-8; printedString[6]=-7; upto=9;}
   for(i=0; i<9; i++){
      if(printedString[i]!=-8 && printedString[i]!=-7){
           if(j==0) printedString[i]=printable[0];
@@ -34,18 +34,18 @@ int printLayout1(int printable[]){    //I first place the parenthesis where need
 
 int printLayout2(int printable[]){
   int i, j=0, upto=7;
-  if(printable[2]<12 && printable[5]<12 && printable[6]<12) return 0;                                              //checking if... 
-  if(printable[2]<12 && printable[5]>11) return 0;                                                                 //...already printed
-  if(printable[2]<12 && printable[6]>11){printedString[0]=-8; printedString[4]=-7; upto+=2;}                       //here i plave the parenthesis in the right spot...
-  if(printable[5]<12 && printable[6]>11){                                                                          //
+  if(printable[2]<Mul && printable[5]<Mul && printable[6]<Mul) return 0;                                              //checking if... 
+  if(printable[2]<Mul && printable[5]>Sub) return 0;                                                                 //...already printed
+  if(printable[2]<Mul && printable[6]>Sub){printedString[0]=-8; printedString[4]=-7; upto+=2;}                       //here i plave the parenthesis in the right spot...
+  if(printable[5]<Mul && printable[6]>Sub){                                                                          //
        if(upto==9)   {printedString[6]=-8; printedString[10]=-7; upto+=2;}                                         //
        else          {printedString[4]=-8; printedString[8]=-7; upto+=2;}                                          //...all the way down here
   }
   if(upto==9){printedString[3]=printable[6]; printedString[1]=printable[2]; printedString[6]=printable[5];}
   if(upto==11){printedString[5]=printable[6]; printedString[2]=printable[2]; printedString[8]=printable[5];}
   for(i=0; i<upto; i++){
-       if(printedString[i]<10 && printedString[i]>0){
-           if(printable[j]<10) printedString[i]=printable[j];
+       if(printedString[i]<Add && printedString[i]>0){
+           if(printable[j]<Add) printedString[i]=printable[j];
            else printedString[i]=printable[j+1];
            j++;
        }
@@ -54,12 +54,11 @@ int printLayout2(int printable[]){
 }
 
 //input is an array of length 7;
-//10:+ 11:- 12:* 13:/
 int math(int arr[]) {
   int i,current=0;
   float stack[4];
   for (i=0; i<7; i++) {
-    if (arr[i] < 10) {
+    if (arr[i] < Add) {
       stack[current] = arr[i];
       current++;
     } else {
@@ -90,10 +89,10 @@ int math(int arr[]) {
 //Rearrange and print reverse polish notation in normal notation, input is a 7 elements array, also checks if string has been already printed 
 void printp(int sequence[]) {
   int i, upto;
-  if(sequence[4]>=10)  upto=printLayout1(sequence);     //print the ab?c?d? form
+  if(sequence[4]>=Add)  upto=printLayout1(sequence);     //print the ab?c?d? form
   else upto=printLayout2(sequence);                     //print the ab?cd?? form
   for(i=0; i<upto; i++){
-    if(printedString[i]<10)  printf("%c", printedString[i]+48);
+    if(printedString[i]<Add)  printf("%c", printedString[i]+48);
     else{
       switch(printedString[i]){
         case Add: printf("+");
@@ -119,11 +118,11 @@ void gennumbers(int input[4], int n) {
       int arr[7]= {input[0], input[1], 0, input[2], 0, input[3], 0};
       int tmp;
       tmp = operators;
-      arr [2] = tmp % 4 + 10;
+      arr [2] = tmp % 4 + Add;
       tmp /= 4;
-      arr [4] = tmp % 4 + 10;
+      arr [4] = tmp % 4 + Add;
       tmp /= 4;
-      arr [6] = tmp % 4 + 10;
+      arr [6] = tmp % 4 + Add;
       if (math(arr)==24) printp(arr);              //ab?c?d?
       swap(arr + 4, arr + 5);
       if (math(arr)==24) printp(arr);              //ab?cd??
